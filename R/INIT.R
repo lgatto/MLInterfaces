bf4652fe3fa7900ed8dc20505cdb9636b85ebadb
stvjc <stvjc@bc3139a8-67e5-0310-9ffc-ced21a209358>
2004-07-23 21:34:07 +0000

    ok
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MLInterfaces@9517 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/INIT.R
+++ R/INIT.R
@@ -10,16 +10,18 @@
 #
 
 setClass("MLLabel", "VIRTUAL")
-setClass("predClass", contains=c("MLLabel", "character"))
-setClass("groupIndex", contains=c("MLLabel", "integer"))
+setClass("predClass", contains=c("MLLabel", "character"),
+	prototype=prototype(""))
+setClass("groupIndex", contains=c("MLLabel", "integer"),
+	prototype=prototype(integer(0)))
 newPredClass <- function(x) new("predClass", x)
 newGroupIndex <- function(x) new("groupIndex", x)
 
 setClass("MLScore", "VIRTUAL")
 setClass("probMat", contains=c("MLScore", "matrix"))
 setClass("qualScore", contains=c("MLScore", "vector"))
-newProbMat <- function(x) new("probMat", x)
-newQualScore <- function(x) new("qualScore", x)
+newProbMat <- function(x) if(length(x)>0)new("probMat", x) else new("probMat")
+newQualScore <- function(x) if(length(x)>0)new("qualScore", x) else new("qualScore")
 
 #
 # the base output representation class now just knows
@@ -29,4 +31,32 @@ newQualScore <- function(x) new("qualScore", x)
 
 setClass("classifOutput", representation(method="character",
 	predLabels="MLLabel", predScores="MLScore", 
-	call="call", classifRobject="ANY", distMat="dist"))
+	call="call", classifRobject="ANY", distMat="dist"),
+		prototype=prototype(method="",
+			predLabels=newPredClass(character(0)),
+			predScores=newQualScore(numeric(0))))
+
+
+setMethod("show", "classifOutput", function(object) {
+	cat("classifOutput instance, method", object@method, "\n")
+	if (object@method == "nnet")
+		print(object@classifRobject)
+	if (length(object@call)>0) print (object@call)
+        if (length(object@predLabels)>0) {
+		cat("predicted class distribution:")
+		print(table(object@predLabels))
+	}
+        if (length(object@predScores)>0 && object@method %in% c("nnet", "knn"))
+           {
+	   if (is(object@predScores,"qualScore"))
+	   	{
+		cat("summary of labeling scores:\n")
+		print(summary(object@predScores))
+		}
+	   else if (is(object@predScores,"probMat"))
+		{
+		cat("summary of class probabilities:\n")
+		print(apply(object@predScores,2,summary))
+		}
+            }
+	})
