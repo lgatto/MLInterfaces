2676b45b85b60150f3ede878b13b64fafd4fb98e
stvjc <stvjc@bc3139a8-67e5-0310-9ffc-ced21a209358>
2005-01-27 20:37:27 +0000

    stat.diag.daB
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MLInterfaces@10937 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/smaInterfaces.R
@@ -0,0 +1,27 @@
+
+
+setGeneric("stat.diag.daB", function(exprObj, classifLab, trainInd, 
+ pool=1, metric="euclidean", ...) standardGeneric("stat.diag.daB") )
+
+setMethod("stat.diag.daB", c("exprSet", "character", "integer", "ANY", "ANY", 
+		"ANY" ),
+		function(exprObj, classifLab, trainInd, pool,  
+                       metric, ...){
+		
+			require(sma) # nice if it had a namespace
+			cl <- exprObj[[classifLab]][trainInd]
+
+			trainDat <- t(exprs(exprObj)[,trainInd])
+			testDat <- t(exprs(exprObj)[,-trainInd])
+
+			dis <- dist(testDat, method=metric)
+	
+			out <- stat.diag.da(trainDat,
+   as.integer(factor(cl)), testDat, pool=pool )
+
+			new("classifOutput", method="stat.diag.da",
+	predLabels=newPredClass(as.character(out$pred)), 
+			trainInds=trainInd, allClass=as.character(exprObj[[classifLab]]),
+        call=match.call(),
+        distMat=dis, RObject=out)	
+})		  
