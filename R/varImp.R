b1519c6d23cc980ebbd35e3e12b398593fd52571
stvjc <stvjc@bc3139a8-67e5-0310-9ffc-ced21a209358>
2005-01-28 15:57:51 +0000

    varImp structures
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MLInterfaces@10944 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/varImp.R
@@ -0,0 +1,51 @@
+
+setClass("varImpStruct", representation(method="character",
+ varnames="character"), contains="matrix")
+
+if(!isGeneric("getVarImp"))setGeneric("getVarImp",
+ function(object)standardGeneric("getVarImp"))
+
+setMethod("getVarImp", "classifOutput", function(object) {
+	if (is(object@RObject,"randomForest")) {
+		imp <- object@RObject$importance
+		return(new("varImpStruct", data.matrix(imp[,-1]), 
+			method="randomForest", varnames= row.names(imp)))
+		}
+	else if (is(object@RObject,"gbm")) {
+		imp <- summary(object@RObject, plotit=FALSE)
+		return(new("varImpStruct", data.matrix(imp[,-1]), 
+                         method="gbm", varnames=as.character(imp[,1])))
+	        }
+	else stop("getVarImp defined only for randomForestB or gbmB output")
+})
+
+setMethod("show", "varImpStruct", function(object) {
+ cat("varImpStruct for method", object@method, "\n")
+ cat("first five varnames:\n")
+ print(object@varnames[1:5])
+ cat("dims importance:\n")
+ print(dim(object@.Data))
+})
+
+setMethod("plot", "varImpStruct", function(x, y, ..., n=20, resolveenv=NULL) {
+        vn <- x@varnames
+        if (!is.null(resolveenv))
+		{
+		nn <- unlist(mget(vn, resolveenv))
+		nn[is.na(nn)] <- vn[is.na(nn)]
+		vn <- nn
+		}
+	if (x@method=="gbm") 
+		{
+		barplot(x@.Data[n:1], names=vn[n:1], horiz=TRUE,
+			xlab="Relative importance")
+		}
+	else if (x@method=="randomForest") 
+		{
+		mda <- x@.Data[,"MeanDecreaseAccuracy"]
+                omda <- order(-mda)[n:1]
+		barplot(mda[omda], names=vn[omda], horiz=TRUE,
+			xlab="Mean decrease in accuracy")
+		}
+})
+           
