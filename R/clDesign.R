496d72bea9a05717375d30c2cd4cad0b5396cb6b
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-11-10 00:49:48 +0000

    not scaling kmeans' partition in partPlot
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MLInterfaces@71108 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/clDesign.R
+++ R/clDesign.R
@@ -45,20 +45,23 @@ setMethod("show", "clusteringOutput", function(object) {
 setMethod("RObject", "clusteringOutput", function(x) x@RObject)
 
 setMethod("plot", "clusteringOutput", function(x, y, ...) {
- opar = par(no.readonly=TRUE)
- on.exit(par(opar))
- par(mfrow=c(2,2))
- NO_PLOT_METHOD = c("kmeans")
- if (x@learnerSchema@mlFunName %in% NO_PLOT_METHOD) {
+  opar = par(no.readonly=TRUE)
+  on.exit(par(opar))
+  par(mfrow=c(2,2))
+  NO_PLOT_METHOD = c("kmeans")
+  if (x@learnerSchema@mlFunName %in% NO_PLOT_METHOD) {
     if (missing(y)) stop("second arg must be matrix with feature data on all records")
     partPlot(y, x@partition, las=2, ...)
-    }
- else if (x@learnerSchema@mlFunName == "pam") plot(RObject(x), which=1, ...)
- else plot(RObject(x), ...)
- plot(x@silhouette, main="silhouette")
- plot(x@prcomp, main="PCA screeplot")
- plot(x@prcomp$x[,1], x@prcomp$x[,2], col=x@partition,
-   xlab="PC1", ylab="PC2", main="PCA colored by partition")
+  }
+  else if (x@learnerSchema@mlFunName == "pam") {
+    plot(RObject(x), which=1, ...)
+  } else {
+    plot(RObject(x), ...)
+  }
+  plot(x@silhouette, main="silhouette")
+  plot(x@prcomp, main="PCA screeplot")
+  plot(x@prcomp$x[,1], x@prcomp$x[,2], col=x@partition,
+       xlab="PC1", ylab="PC2", main="PCA colored by partition")
 })
 
 
--- R/partPlot.R
+++ R/partPlot.R
@@ -1,8 +1,10 @@
-partPlot = function(x, p, doXax=TRUE, ...) {
- rzsco = function(x) (x-median(x))/mad(x)
- d = cbind(as.numeric(p), data.matrix(x))
- d = apply(d, 2, rzsco)
- d = d[order(d[,1]),]
- image(t(d), axes=FALSE, xlab="part.; features", ylab="cases", ...)
- if (doXax) axis(1, at=seq(.01,.99,len=ncol(d)), c("part.", colnames(d)[-1]))
+partPlot <- function(x, p, doXax=TRUE, ...) {
+  ## Not scaling kmeans' partitions p, only matrix x - L. Gatto 10 Nov 2012
+  rzsco <- function(x) (x-median(x))/mad(x)
+  x <- data.matrix(x)
+  x <- apply(x, 2, rzsco)
+  d <- cbind(as.numeric(p), x)
+  d <- d[order(d[,1]),]
+  image(t(d), axes=FALSE, xlab="part.; features", ylab="cases", ...)
+  if (doXax) axis(1, at=seq(.01,.99,len=ncol(d)), c("part.", colnames(d)[-1]))
 }
