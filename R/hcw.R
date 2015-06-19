962b83aba5ded17e79957845ac103fd4f265aa0a
stvjc <stvjc@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-06-19 14:29:06 +0000

    new widget added
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MLInterfaces@105165 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/hcw.R
@@ -0,0 +1,63 @@
+hclustWidget = function(mat, featureName="feature", minfeats=2, auxdf=NULL) {
+#
+# software that defines interactive browser interface
+# to approaches to clustering the rows of a matrix
+#
+#
+# to use, run library(shiny); library(cluster); library(fpc); library(MLInterfaces); run(hclustWidget(mat))
+#
+ #require("cluster")
+ #require("fpc")
+ #require("ggvis")
+ shinyApp(ui = fluidPage(
+  fluidRow(
+   column(2,  numericInput("ngenes", label = paste0("N ", featureName, "s"), 
+     minfeats, min = minfeats, max = nrow(mat))),
+   column(2,  selectInput("distmeth", label = "Distance method:",
+               choices = c("euclidean", "maximum", "manhattan",
+               "binary"), selected = "euclidean")),
+
+   column(2,  selectInput("fusemeth", label = "Agglomeration method:",
+               choices = c("complete", "average", "ward.D2", "single",
+                   "median", "centroid"), selected="complete")),
+   column(2,  numericInput("numclus", label = "K:", 2, min = 1, max = nrow(mat)/2))
+          ),
+  fluidRow(column(7, plotOutput("tree"))),
+  fluidRow(column(7, ggvisOutput("pcp")))
+ ), server= function(input, output, session) {
+    output$tree <- renderPlot({
+dm = dist(mat[,1:input$ngenes], method=input$distmeth)
+sink(tempfile())
+cb <- clusterboot(dm, clustermethod=hclustCBI, method=input$fusemeth, k=input$numclus, showplots=FALSE, scaling=FALSE)
+sink(NULL)
+      dend = hclust( dm, method=input$fusemeth )
+      par(mar=c(3,3,3,1))
+      plot(dend, main=paste0("Boot. Jacc. at k=", input$numclus, ": ",
+        paste(round(cb$bootmean,2), collapse=", ")), xlab=" ")
+    })
+    P1 <- reactive({
+           all_values <- function(x) {
+             if(is.null(x)) return(NULL)
+             row <- pcdf[pcdf$rowid == x$rowid, ]
+             paste0(names(row), ": ", format(row), collapse = "<br />")
+           }
+
+      pc = prcomp(mat[,1:input$ngenes])$x
+      dm = dist(mat[,1:input$ngenes], method=input$distmeth)
+
+
+      dend = hclust( dm, method=input$fusemeth )
+      ct = cutree(dend, k=input$numclus)
+      pcdf = data.frame(PC1=pc[,1], PC2=pc[,2], #tiss=pData(tiss)$Tissue,
+         rowid=1:nrow(pc), assigned=factor(ct))
+      if (!is.null(auxdf)) {
+         if ((nrow(auxdf) == nrow(pcdf))) pcdf = cbind(pcdf, auxdf)
+           else message("nrow(auxdf) != nrow(mat), ignoring auxdf")
+         }
+      pcdf %>% ggvis(~PC1, ~PC2, key := ~rowid, fill = ~assigned) %>% layer_points() %>%
+               add_tooltip(all_values, "hover") 
+#      pairs(pc[,1:3], col=ct, pch=19, cex=1.5)
+      }) 
+      P1 %>% bind_shiny("pcp")
+} )
+}
